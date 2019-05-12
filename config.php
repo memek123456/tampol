a2dbe02860acd6acb87ebd8c8948dd9fd2169d89
memek123456 <46232817+memek123456@users.noreply.github.com>
2019-05-13 04:39:38 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+/**
+untuk pengguna input data yang di butuhkan
+$rd_email => isi nomer yang sudah di daftar kan
+$rd_pass  => isi dengan password kalian
+
+•note jika gagal login ubah di bagian rd_email
+isi dengan email/nomer jika gagal
+**/
+
+$rd_email="kenjikazama7@gmail.com";
+$rd_pass="kontol123";
